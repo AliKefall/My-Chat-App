110fb51038125b54e756808e20ad2f759afdbc26
AliKefall <osman.ali_shen@outlook.com>
2025-08-26 22:35:34 +0300

    Starting over from scratch

--- backend/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/AliKefall/Portfolio1
-
-go 1.24.0
-
-require github.com/gorilla/websocket v1.5.3 // indirect
--- frontend/src/components/App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import './App.css';
-import { Route, Switch } from 'react-rooter-dom';
-import Header from './components/Header/Header';
-import ChatPage from "./components/Chat/Chatpage";
-import LoginPage from './components/Login/LoginPage';
-import { ProtectedRoute } from './authorization/protected.route';
